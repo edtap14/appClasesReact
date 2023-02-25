bf5acfcff46e111dd595ae547f10edcdb3550c7d
Edgar Tapia <edgar.tapia@outlook.es>
2023-02-25 01:27:19 -0600

    generamos la llamada a la Api de productos

--- src/views/Productos/Productos.jsx
+++ src/views/Productos/Productos.jsx
@@ -1,10 +1,37 @@
-import './Productos.css'
+import './Productos.css';
+import { url } from '../../Varibles/variables';
+import { useEffect, useState } from 'react';
 
 export const Productos = () => {
-    return (
-        <div className="Productos">
-            <h1 className='h3Productos'>Lista de productos</h1>
-            <li className='pProductos'>Aquí debería ir un producto (componente)</li>
-        </div>
-    )
-}
+	const [products, setProducts] = useState({
+		limit: 0,
+		products: [],
+		skip: 0,
+		total: 0,
+	});
+
+	const getProductos = () => {
+		try {
+			fetch('https://dummyjson.com/products')
+				.then((res) => res.json())
+				.then((data) => setProducts(data));
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getProductos();
+	}, []);
+
+	console.log('Resultado final:', products);
+
+	return (
+		<div className='Productos'>
+			<h1 className='h3Productos'>Lista de productos</h1>
+			<li className='pProductos'>
+				Aquí debería ir un producto (componente)
+			</li>
+		</div>
+	);
+};
