8521dc0ea8278d7517851189aec5b75e0144ab7a
anayaricardo <einstein.letsgetrelative@gmail.com>
2023-02-26 23:19:27 -0600

    actualización usuarios

--- src/Views/Usuarios/Usuarios.jsx
+++ src/Views/Usuarios/Usuarios.jsx
@@ -1,6 +1,14 @@
-import './Usuarios.css'
+import './Usuarios.css';
+import { useEffect, useState } from 'react';
 
 export const Usuarios = () => {
+
+    const [products, setProducts] = useState({
+        id: null,
+        
+
+    });
+
     return (
         <div className="Usuarios">
             <h1 className='h3Usuarios'>Mi perfil</h1>
