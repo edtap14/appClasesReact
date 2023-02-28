35bfac026ab0448c76eae420f71b72f52caf06b7
anayaricardo <einstein.letsgetrelative@gmail.com>
2023-02-27 21:21:59 -0600

    users props

--- src/Views/Usuarios/Usuarios.jsx
+++ src/Views/Usuarios/Usuarios.jsx
@@ -1,40 +1,53 @@
-import './Usuarios.css';
-import { useEffect, useState } from 'react';
+import "./Usuarios.css";
+import { useEffect, useState } from "react";
 
 export const Usuarios = () => {
+  const [users, setUsers] = useState({
+    id: 0,
+    firstName: "",
+    lastName: "",
+    maidenName: "",
+    age: 0,
+    gender: "",
+    image: "",
+  });
 
-    const [users, setUsers] = useState({
-        id: 0,
-        firstName:'',
-        lastName:'',
-        maidenName:'',
-        age:0,
-        gender:'',
-        image:'',      
-    });
+  console.log("Inicial", users);
 
-    console.log('Inicial', users)
-
-    const getUsers = () => {
-        try {
-            fetch('https://dummyjson.com/users')
-            .then((res) => res.json())
-            .then((data) => setUsers(data))
-        }catch (error) {
-            console.log(error)
-        }
+  const getUsers = () => {
+    try {
+      fetch("https://dummyjson.com/users")
+        .then((res) => res.json())
+        .then((data) => setUsers(data));
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        getUsers();
-      }, []);
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-      console.log('Resultado final',users)
+  console.log("Resultado final", users);
 
-    return (
-        <div className="Usuarios">
-            <h1 className='h3Usuarios'>Mi perfil</h1>
-            <li className='pUsuarios'>Aquí deberían ir los datos del usuario (componente)</li>
-        </div>
-    )
-}
+  const usuario = (users) => {
+    return {
+      id: 0,
+      firstName: "",
+      lastName: "",
+      maidenName: "",
+      age: 0,
+      gender: "",
+      image: "",
+    };
+  };
+
+  return (
+    <div className="Usuarios">
+      <h1 className="h3Usuarios">Mi perfil</h1>
+      <li className="pUsuarios">
+        Aquí deberían ir los datos del usuario (componente)
+      </li>
+    </div>
+  );
+};
